ea363bfa095aece55913b0196756b54558652487
VinnieIT <mwangivin3@gmail.com>
2021-08-24 20:39:00 +0300

    Hero tests

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,6 +1,7 @@
 package models;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -18,26 +19,47 @@ class HeroTest {
 
     @Test
     void getName() {
+        Hero hero = new Hero("Vincent", 22, "SuperSpreader", "Vaccine");
+        String name = hero.getName();
+        Assertions.assertTrue(true,name );
     }
 
     @Test
     void getAge() {
+        Hero hero = new Hero("Vincent", 22, "SuperSpreader", "Vaccine");
+        String age = hero.getWeakness();
+        Assertions.assertTrue(true,age );
     }
 
     @Test
     void getHeroPower() {
+        Hero hero = new Hero("Vincent", 22, "SuperSpreader", "Vaccine");
+        String power = hero.getHeroPower();
+        Assertions.assertTrue(true,power );
     }
 
     @Test
     void getWeakness() {
+        Hero hero = new Hero("Vincent", 22, "SuperSpreader", "Vaccine");
+        String weakness = hero.getWeakness();
+        Assertions.assertTrue(true,weakness );
     }
 
     @Test
     void getAll() {
+        Hero hero = new Hero("Vincent", 22, "SuperSpreader", "Vaccine");
+        Hero hero2 = new Hero("James", 18, "SuperDrinker", "Sobriety");
+        assertTrue(Hero.getAll().contains(hero));
+        assertTrue(Hero.getAll().contains(hero2));
     }
 
     @Test
     void clearAllHeroes() {
+        Hero hero = new Hero("Vincent", 22, "SuperSpreader", "Vaccine");
+        Hero hero2 = new Hero("James", 18, "SuperDrinker", "Sobriety");
+        Hero.clearAllHeroes();
+        Assertions.assertEquals(0,Hero.getAll().size());
+
     }
 
     @Test
